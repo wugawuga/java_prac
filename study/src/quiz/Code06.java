8446f858ae121fe88dd55a81fd96666e0bb28512
Wugawuga <dnr3655@naver.com>
2021-12-05 19:19:31 +0900

    변경 - java 알고리즘 연습 Code06

--- study/src/quiz/Code06.java
+++ study/src/quiz/Code06.java
@@ -1,16 +1,44 @@
 package quiz;
 
+import java.util.*;
+
 public class Code06 {
 	
-	public static void main(String args[]){
-        
-        int [] numbers = new int[] {1,2,3,4};
-        for(int i = 0; i < numbers.length; i++) {
-        	for(int j = 1; j < numbers.length; j++) {
-        	int dap = numbers[i]+numbers[j];
-        	System.out.println(dap);
-        	}
-        }
-        
-    }
+	public int[] solution(int[] numbers) {
+		Arrays.sort(numbers);
+		List<Integer> tmpList = new ArrayList<>();
+		List<Integer> sumList = new ArrayList<>();
+
+//      배열의 중복을 제거하고 중복된값은 합배열에 추가해주는 방법.
+		tmpList.add(numbers[0]);
+		for (int i = 0; i < numbers.length - 1; i++) {
+			if (numbers[i] != numbers[i + 1]) {
+				tmpList.add(numbers[i + 1]);
+
+			} else {
+				if (!sumList.contains(numbers[i] * 2)) {
+					sumList.add(numbers[i] * 2);
+				}
+			}
+		}
+		
+        //중복이 제거된 tmpList를 활용하여 더한값을 sumList에 넣어주는 방법.
+		for (int i = 0; i < tmpList.size() - 1; i++) {
+			for (int j = i + 1; j < tmpList.size(); j++) {
+				int sum = tmpList.get(i) + tmpList.get(j);
+				if (!sumList.contains(sum)) {
+					sumList.add(sum);
+				}
+			}
+		}
+		
+		Collections.sort(sumList);
+		int[] answer = new int[sumList.size()];
+		int k = 0;
+		for (int tmp : sumList) {
+			answer[k++] = tmp;
+		}
+		return answer;
+	}
+	// 유캔두 21-12-05!!!알고리즘
 }
